6adbfc0871ac9f33be1ac01836c811f770f830db
Chenxi <835318109@qq.com>
2019-05-28 20:44:43 +0800

    commit message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace vscode_dotnet_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test((a, b) => "11");
+        }
+
+        private static void Test(Expression<Func<string, string, string>> ex)
+        {
+            //....
+        }
+    }
+}
